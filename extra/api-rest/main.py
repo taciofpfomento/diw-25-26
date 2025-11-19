ee2595769b7e3ff7fb700f73339118a92f5f9afc
Tacio Camba Espi <tcamba@fpfomento.com>
2025-11-19 18:51:44 +0100

    feat(sass): add new activities and examples for Tema 5

--- extra/api-rest/main.py
+++ extra/api-rest/main.py
@@ -1,13 +1,32 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/{adjetivo}")
+def read_root(adjetivo: str):
+    index = f"""
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Prueba API</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  HOLA MUNDO {adjetivo}
+</body>
+
+</html>
+"""
+
+    return HTMLResponse(index)
 
 
 @app.get("/items/{item_id}")
